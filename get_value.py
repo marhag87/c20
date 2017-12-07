aba334c0ecec9628bdbf27efc56721a4429cd940
Takeshi <6172961+propyless@users.noreply.github.com>
2017-12-07 21:27:55 +0100

    Add functionality to get any exchange rate (#1)
    * Add functionality to get any exchange rate
    
    * Make USD as currency default

--- get_value.py
+++ get_value.py
@@ -7,14 +7,17 @@ from pathlib import Path
 home = str(Path.home())
 config = load_config(f'{home}/.config/c20.yaml')
 num_tokens = config.get('num_tokens')
-
+currency = config.get('currency', 'USD').upper()
+  
 # Get token value
 response = requests.get('https://us-central1-cryptodash1.cloudfunctions.net/fundValue')
 value_per_token = response.json().get('nav_per_token')
 
-# Get dollar to sek value
-response = requests.get('https://api.fixer.io/latest?base=USD')
-sek = response.json().get('rates').get('SEK')
-
-
-print(int(value_per_token*num_tokens*sek))
+if currency == 'USD':
+  exchange_rate = 1
+else:
+  # Get dollar to chosen currency exchange rate
+  response = requests.get('https://api.fixer.io/latest?base=USD')
+  exchange_rate = response.json().get('rates').get(currency)
+ 
+print(int(value_per_token*num_tokens*exchange_rate))
