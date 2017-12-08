ad3fc833860003a4ed2b8b8eb8e85252cc275e27
Takeshi <6172961+propyless@users.noreply.github.com>
2017-12-08 10:47:08 +0100

    Adds value increase from investment (#2)
    * Adds value increase from investment
    
    * Adds custom format string

--- README.md
+++ README.md
@@ -3,6 +3,48 @@ c20
 
 Keep track of the value of your https://crypto20.com/ investment.
 
+Config
+------
+```
+num_tokens: <int> - Number of tokens owned>
+currency: <string> - Currency to show token value in (eg. GBP) Default is 'USD'
+total_investment: <int> - Total amount invested in C20 *Should be the same currency as {currency}*)
+status_format: <string> - Custom format string for the message
+```
+
+Format String Options
+---------------------
+```
+value_per_token - Token NAV value
+num_tokens - Number of tokens you have specified in config
+currency - The currency you have chosen
+total_investment - The amount of money you invested
+exchange_rate - The current exchange rate to your currency from USD
+increase_num - The value increase from your investment cost to the current value_per_token * num_tokens
+increase_percent - The percentage increase to your investment
+precent_prefix - Adds a + or a minus infront of the increase_percent (eg. +23.33%, -34.44%)
+```
+
+Format String Examples
+----------------------
+Some examples when building your string format.
+
+This is the default string format if none is specified
+```
+'C20: NAV: {value_per_token} USD - Value: {curr_token_value} {currency} ({percent_prefix}{increase_percent})'
+C20: NAV: 1.3022 USD - Value: 12990.78 SEK (+36.5)
+```
+
+```
+'C20: Value: {curr_token_value} {currency} ({percent_prefix}{increase_percent}%)'
+C20: Value: 12990.78 SEK (+36.5%)
+```
+
+```
+'C20: NAV: {value_per_token} - Tokens: {num_tokens} - Value: {curr_token_value}{currency}'
+C20: NAV: 1.3005 - Tokens: 1181 - Value: 12973.82SEK
+```
+
 Usage
 -----
 ```
--- get_value.py
+++ get_value.py
@@ -3,21 +3,42 @@ import requests
 from pyyamlconfig import load_config
 from pathlib import Path
 
+# default message
+default_msg = 'C20: Value: {curr_token_value} {currency} - Inc: {increase_num} {currency} ({percent_prefix}{increase_percent}%)'
+
 # Get tokens
+data = dict()
 home = str(Path.home())
 config = load_config(f'{home}/.config/c20.yaml')
-num_tokens = config.get('num_tokens')
-currency = config.get('currency', 'USD').upper()
-  
+status_fmt = config.get('status_format', default_msg)
+data['num_tokens'] = config.get('num_tokens')
+data['total_investment'] = config.get('init_investment', False)
+data['currency'] = config.get('currency', 'USD').upper()
+
 # Get token value
 response = requests.get('https://us-central1-cryptodash1.cloudfunctions.net/fundValue')
-value_per_token = response.json().get('nav_per_token')
+data['value_per_token'] = response.json().get('nav_per_token')
 
-if currency == 'USD':
-  exchange_rate = 1
+if data['currency'] == 'USD':
+  data['exchange_rate'] = 1
 else:
   # Get dollar to chosen currency exchange rate
   response = requests.get('https://api.fixer.io/latest?base=USD')
-  exchange_rate = response.json().get('rates').get(currency)
+  data['exchange_rate'] = response.json().get('rates').get(data['currency'])
+
+# Calculate token val
+data['curr_token_value'] = round(data['value_per_token']*data['num_tokens']*data['exchange_rate'], 2)
  
-print(int(value_per_token*num_tokens*exchange_rate))
+# Calculate value increase from investment amount
+if data['total_investment']:
+  data['increase_num'] = round(data['curr_token_value'] - data['total_investment'], 2)
+  data['increase_percent'] = round((( \
+    float(data['curr_token_value'])/ \
+    float(data['total_investment'])) \
+    - 1)*100, 1)
+
+  data['percent_prefix'] =''
+  if data['increase_percent'] > 0:
+    data['percent_prefix'] = '+'
+
+print(status_fmt.format(**data))
